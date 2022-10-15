e7258310d71f6951fc7a97666f8aebc98cd49374
SerMikh1981 <ser.mikh1981@gmail.com>
2022-10-16 01:52:47 +0500

    new import patched

--- api_yamdb/reviews/management/commands/import_test_data.py
+++ api_yamdb/reviews/management/commands/import_test_data.py
@@ -14,146 +14,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fill_test_data()
-        self.stdout.write("Test data loaded.")
+        self.stdout.write('Test data loaded.')
 
 
-def fill_table(db, cursor, table, to_db):
+def fill_table_from_csv(db, cursor, filename):
     """Clear table and fill data."""
+
+    table = filename.split('.')[0]
     cursor.execute(f"DELETE FROM {table}")
-    fields = ",".join("?" * len(to_db[0]))
-    cursor.executemany(f"INSERT INTO {table} VALUES ({fields});", to_db)
+    with open(os.path.join(settings.STATICFILES_DIRS_DATA, filename), "r",
+              encoding="utf8") as csv_data:
+        dr = csv.DictReader(csv_data, delimiter=";")
+        for i in dr:
+            keys = ",".join(i.keys())
+            values = ",".join("'" + item + "'" for item in i.values())
+            cursor.execute(f"INSERT INTO {table} ({keys}) VALUES ({values})")
     db.commit()
-
-
-def get_category_data_from_csv():
-    """Prepare data to table reviews_category."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "category.csv"),
-        "r",
-        encoding="utf8",
-    ) as category_data:
-        dr = csv.DictReader(category_data, delimiter=";")
-        to_db = [(i["id"], i["name"], i["slug"]) for i in dr]
-    return to_db
-
-
-def get_genre_data_from_csv():
-    """Prepare data to table reviews_genre."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "genre.csv"),
-        "r",
-        encoding="utf8",
-    ) as genre_data:
-        dr = csv.DictReader(genre_data, delimiter=";")
-        to_db = [(i["id"], i["name"], i["slug"]) for i in dr]
-    return to_db
-
-
-def get_title_data_from_csv():
-    """Prepare data to table reviews_title."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "titles.csv"),
-        "r",
-        encoding="utf8",
-    ) as title_data:
-        dr = csv.DictReader(title_data, delimiter=";")
-        to_db = [
-            (i["id"], i["name"], i["year"], "", i["category"]) for i in dr
-        ]
-    return to_db
-
-
-def get_genre_title_data_from_csv():
-    """Prepare data to table reviews_title_genre."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "genre_title.csv"),
-        "r",
-        encoding="utf8",
-    ) as genre_titles_data:
-        dr = csv.DictReader(genre_titles_data, delimiter=";")
-        to_db = [(i["id"], i["title_id"], i["genre_id"]) for i in dr]
-    return to_db
-
-
-def get_review_data_from_csv():
-    """Prepare data to table reviews_review."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "review.csv"),
-        "r",
-        encoding="utf8",
-    ) as review_data:
-        dr = csv.DictReader(review_data, delimiter=";")
-        to_db = [
-            (
-                i["id"],
-                i["text"],
-                i["score"],
-                i["pub_date"],
-                i["author"],
-                i["title_id"],
-            )
-            for i in dr
-        ]
-    return to_db
-
-
-def get_comment_data_from_csv():
-    """Prepare data to table reviews_comment."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "comments.csv"),
-        "r",
-        encoding="utf8",
-    ) as comments_data:
-        dr = csv.DictReader(comments_data, delimiter=";")
-        to_db = [
-            (i["id"], i["text"], i["pub_date"], i["author"], i["review_id"])
-            for i in dr
-        ]
-    return to_db
-
-
-def get_user_data_from_csv():
-    """Prepare data to table users_user."""
-    with open(
-        os.path.join(settings.STATICFILES_DIRS_DATA, "users.csv"),
-        "r",
-        encoding="utf8",
-    ) as users_data:
-        dr = csv.DictReader(users_data, delimiter=";")
-        to_db = [
-            (
-                i["id"],
-                "",
-                "",
-                "",
-                i["username"],
-                i["first_name"],
-                i["last_name"],
-                "",
-                True,
-                "",
-                i["role"],
-                i["email"],
-                i["bio"],
-            )
-            for i in dr
-        ]
-    return to_db
 
 
 def fill_test_data():
     """Iterate for all tables, call funcs to prepare and fill data in db."""
     db = sqlite3.connect(os.path.join(settings.BASE_DIR, "db.sqlite3"))
     cursor = db.cursor()
-    get_data_csv = {
-        "reviews_category": get_category_data_from_csv,
-        "reviews_genre": get_genre_data_from_csv,
-        "reviews_title": get_title_data_from_csv,
-        "reviews_title_genre": get_genre_title_data_from_csv,
-        "reviews_review": get_review_data_from_csv,
-        "reviews_comment": get_comment_data_from_csv,
-        "users_user": get_user_data_from_csv,
-    }
-    for table in get_data_csv:
-        data = get_data_csv[table]()
-        fill_table(db, cursor, table, data)
+    for filename in os.listdir(settings.STATICFILES_DIRS_DATA):
+        fill_table_from_csv(db, cursor, filename)
